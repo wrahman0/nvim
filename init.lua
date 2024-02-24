7b3c406c198eecd6fc45d129f2050c9f9ae1aa4d
Wasiur Rahman <wrahman0@gmail.com>
2024-02-24 14:28:34 -0500

    Add vim0options

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
